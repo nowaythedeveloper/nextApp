bb1b2fce9e925ef5f9cd30e1e0e4d292263e0834
VodenikovNI <VodenikovNI@mosreg.ru>
2022-09-10 14:40:02 +0300

    linter

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:*',
+  //       // source: '/:all*(svg|jpg|png|js)',
+  //       locale: false,
+  //       headers: [
+  //         {
+  //           key: 'Cache-Control',
+  //           value: 'public, max-age=9999999999, must-revalidate',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+};
